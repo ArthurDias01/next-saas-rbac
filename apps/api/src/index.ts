ac4067e3688d48a1e1827a9edb0520ef00ac23aa
ArthurDias01 <arthursantos01@gmail.com>
2024-05-15 11:34:57 -0300

    turborepo monorepo config

---
+++ apps/api/src/index.ts
@@ -0,0 +1,10 @@
+import { defineAbilityFor, projectSchema } from '@saas/auth'
+
+const ability = defineAbilityFor({ role: 'MEMBER', id: '1'})
+
+const project = projectSchema.parse({ id: 'user-id', ownerId: '1' })
+
+console.log(ability.can('get', 'Billing'))
+console.log(ability.can('create','Invite'))
+console.log(ability.can('delete', project))
+
